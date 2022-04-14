9810efc4feecfe589c29effdef26e8025ada77fc
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-04-14 22:40:22 +0600

    Home react bootstrap slider added

--- src/components/Header/Home/Home.js
+++ src/components/Header/Home/Home.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Services from "../../Services/Services";
+import Banner from "./Banner/Banner";
 
 const Home = () => {
   return (
     <div>
-      <div className="container py-5 my-1 ">
+      {/* <div className="container py-5 my-1 ">
         <h1 className="border-start border-5 border-primary ps-2">
           Welcome Home
         </h1>
-      </div>
+      </div> */}
+      <section>
+        <Banner></Banner>
+      </section>
       <div className="container">
         <div className="my-5">
           <Services></Services>
